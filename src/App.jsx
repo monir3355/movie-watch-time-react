9683cd63707b1369750027974592b1121102eb9a
monir3355 <16173103036@cse.bubt.edu.bd>
2023-03-30 02:52:45 +0600

    add break time and taosty msg

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './components/Home/Home';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 function App() {
 
   return (
     <div className="App">
       <Home></Home>
+      <ToastContainer></ToastContainer>
     </div>
   )
 }
--- src/components/SideWatchTime/SideWatchTime.jsx
+++ src/components/SideWatchTime/SideWatchTime.jsx
@@ -1,15 +1,38 @@
 import React, { useEffect, useState } from "react";
-
+import { ToastContainer, toast } from 'react-toastify';
 const SideWatchTime = ({ newWTime }) => {
   const [time, setTime] = useState(newWTime);
+  const [breakTime, setBreakTime] = useState(null);
+// console.log(breakTime);
+  const handleBreakTime =(time)=>{
+    localStorage.setItem("break-time", time)
+    setBreakTime(time);
+  }
+  const handleComplete=()=>{
+    toast("Wow I Saw Full Movie!!");
+  }
   useEffect(() => {
     const getWatchTime = localStorage.getItem("watch-time");
     setTime(getWatchTime);
+    const breakTime = localStorage.getItem("break-time");
+    if(breakTime){
+      setBreakTime(breakTime);
+    }
+    else{
+      setBreakTime(0);
+    }
   }, [newWTime]);
   return (
     <>
       <h5>Total Watch Time</h5>
-      <input type="text" className="form-control" value={time} />
+      <input type="text" className="form-control my-3" value={time} />
+      <div className="d-grid gap-2 d-md-flex justify-content-start">
+        <button onClick={()=>handleBreakTime(15)} className="btn btn-primary" type="button">15</button>
+        <button onClick={()=>handleBreakTime(30)} className="btn btn-primary" type="button">30</button>
+        <button onClick={()=>handleBreakTime(45)} className="btn btn-primary" type="button">45</button>
+      </div>
+      <input type="text" className="form-control my-3" value={breakTime} />
+      <button onClick={handleComplete} className="btn btn-primary" type="button">Complete</button>
     </>
   );
 };
